3fda3ce519d99b9822e0773a0cc6dcc836ec9cc2
alexx-frcs <alex.frcs22@gmail.com>
2024-02-24 16:38:50 +0100

    train

--- src/train.py
+++ src/train.py
@@ -2,6 +2,8 @@ from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
 import torch
 import torch.nn as nn 
+import random
+import numpy as np
 
 device = torch.device("cpu")
 
@@ -16,24 +18,149 @@ env = TimeLimit(
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
 class ProjectAgent:
+
+    def __init__(self, config, model):
+        device = "cpu"
+        self.gamma = config['gamma']
+        self.batch_size = config['batch_size']
+        self.nb_actions = config['nb_actions']
+        self.memory = ReplayBuffer(config['buffer_size'], device)
+        self.epsilon_max = config['epsilon_max']
+        self.epsilon_min = config['epsilon_min']
+        self.epsilon_stop = config['epsilon_decay_period']
+        self.epsilon_delay = config['epsilon_delay_decay']
+        self.epsilon_step = (self.epsilon_max-self.epsilon_min)/self.epsilon_stop
+        self.model = model 
+        self.criterion = torch.nn.MSELoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=config['learning_rate'])
+        self.path = 'src/DQNagent.pt'
+
     def act(self, observation, use_random=False):
-        return 0
+        if use_random:
+            return np.random.choice(env.action_space.n)
+        else:
+            return greedy_action(self.model, observation)
 
     def save(self, path):
-        pass
+        print("Saving model ...")
+        torch.save({
+                    'model_state_dict': self.model.state_dict(),
+                    }, path)
+
 
     def load(self):
-        pass
+        self.model.load_state_dict(torch.load(self.path, map_location='cpu'))
+        self.model.eval()
+        
 
-class DQNAgent():
-    def __init__(self):
-        self.state_dim = env.observation_space.shape[0]
-        self.n_action = env.action_space.n 
-        self.nb_neurons=24
+
+    
+    def gradient_step(self):
+        if len(self.memory) > self.batch_size:
+            X, A, R, Y, D = self.memory.sample(self.batch_size)
+            QYmax = self.model(Y).max(1)[0].detach()
+            #update = torch.addcmul(R, self.gamma, 1-D, QYmax)
+            update = torch.addcmul(R, 1-D, QYmax, value=self.gamma)
+            QXA = self.model(X).gather(1, A.to(torch.long).unsqueeze(1))
+            loss = self.criterion(QXA, update.unsqueeze(1))
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step() 
+    
+    def train(self, env, max_episode):
+        episode_return = []
+        episode = 0
+        episode_cum_reward = 0
+        state, _ = env.reset()
+        epsilon = self.epsilon_max
+        step = 0
+
+        while episode < max_episode:
+            # update epsilon
+            if step > self.epsilon_delay:
+                epsilon = max(self.epsilon_min, epsilon-self.epsilon_step)
+
+            # select epsilon-greedy action
+            if np.random.rand() < epsilon:
+                action = env.action_space.sample()
+            else:
+                action = greedy_action(self.model, state)
+
+            # step
+            next_state, reward, done, trunc, _ = env.step(action)
+            self.memory.append(state, action, reward, next_state, done)
+            episode_cum_reward += reward
+
+            # train
+            self.gradient_step()
+
+            # next transition
+            step += 1
+            if done:
+                episode += 1
+                print("Episode ", '{:3d}'.format(episode), 
+                      ", epsilon ", '{:6.2f}'.format(epsilon), 
+                      ", batch size ", '{:5d}'.format(len(self.memory)), 
+                      ", episode return ", '{:4.1f}'.format(episode_cum_reward),
+                      sep='')
+                state, _ = env.reset()
+                episode_return.append(episode_cum_reward)
+                episode_cum_reward = 0
+            else:
+                state = next_state
+
+        return episode_return
+        
+
+
+
+
+def greedy_action(network, state):
+    device = "cpu"
+    with torch.no_grad():
+        Q = network(torch.Tensor(state).unsqueeze(0).to(device))
+        return torch.argmax(Q).item()
+        
+
+class ReplayBuffer:
+    def __init__(self, capacity, device):
+        self.capacity = capacity # capacity of the buffer
+        self.data = []
+        self.index = 0 # index of the next cell to be filled
+        self.device = device
+    def append(self, s, a, r, s_, d):
+        if len(self.data) < self.capacity:
+            self.data.append(None)
+        self.data[self.index] = (s, a, r, s_, d)
+        self.index = (self.index + 1) % self.capacity
+    def sample(self, batch_size):
+        batch = random.sample(self.data, batch_size)
+        return list(map(lambda x:torch.Tensor(np.array(x)).to(self.device), list(zip(*batch))))
+    def __len__(self):
+        return len(self.data)
     
 
-        DQN = torch.nn.Sequential(nn.Linear(self.state_dim, self.nb_neurons),
-                                nn.ReLU(),
-                                nn.Linear(self.nb_neurons, self.nb_neurons),
-                                nn.ReLU(), 
-                                nn.Linear(self.nb_neurons, self.n_action)).to(device)
+state_dim = env.observation_space.shape[0]
+n_action = env.action_space.n 
+nb_neurons=24
+DQN = torch.nn.Sequential(nn.Linear(state_dim, nb_neurons),
+                          nn.ReLU(),
+                          nn.Linear(nb_neurons, nb_neurons),
+                          nn.ReLU(), 
+                          nn.Linear(nb_neurons, n_action)).to(device)
+
+# DQN config
+config = {'nb_actions': env.action_space.n,
+          'learning_rate': 0.001,
+          'gamma': 0.95,
+          'buffer_size': 1000000,
+          'epsilon_min': 0.01,
+          'epsilon_max': 1.,
+          'epsilon_decay_period': 1000,
+          'epsilon_delay_decay': 20,
+          'batch_size': 20}
+
+# Train agent
+agent = ProjectAgent(config, DQN)
+agent.save('src/DQNagent.pt')
+scores = agent.train(env, 200)
